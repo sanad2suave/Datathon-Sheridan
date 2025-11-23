33c94a4e54392065e6b4133b7b3166e8f0668474
alexwmson <160697078+alexwmson@users.noreply.github.com>
2025-11-23 16:32:59 -0500

    Update explore.tsx

--- frontend/app/(tabs)/explore.tsx
+++ frontend/app/(tabs)/explore.tsx
@@ -188,7 +188,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 20,
-    paddingTop: 80,
+    paddingTop: 20,
     paddingBottom: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#E0E0E0',
